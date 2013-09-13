ec708987ea4728e98fbd87a5ec686be111c525ce
Fernando Pereira <fernandopereirala@gmail.com>
2013-09-13 15:07:20 -0300

    Foram realizadas diversas modificações que resultaram num pesquisa mais inteligente. Agora a gem sabe pesquisar por um endereço retornando um bloco de vários ceps ou vazio caso não encontre nada para ambas as pesquisas(cep ou endereço).Os testes para os dois casos também foram resolvidos. Com esse novo mecanismo usando a criação de hash é possível ele resolver caso existe alguma nova informação adicinonada a pagina de consulta.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 Para consultar endereço, pelo cep:
 
 ```ruby
-Cep.new.address("02045970")
+Cep.new.cep("02045970")
 
 # {
 #    logradouro: "Rua Guajurus, 69",
@@ -14,4 +14,26 @@ Cep.new.address("02045970")
 #    estado: "SP",
 #    cep: "02045970"
 # }
+```
+== Endereço
+
+= CEP
+
+Para consultar cep, pelo endereço:
+
+```ruby
+Cep.new.address("Avenida Conde da Boa Vista Recife")
+
+#[{:tipo_logradouro=>"Avenida", 
+#  :logradouro=>"Avenida Conde da Boa Vista - até 1016 - lado par", 
+#  :bairro=>"Boa Vista", 
+#  :localidade=>"Recife", 
+#  :uf=>"PE", 
+#  :cep=>"50060004"}, 
+# {:tipo_logradouro=>"Avenida", 
+#  :logradouro=>"Avenida Conde da Boa Vista - até 1017 - lado ímpar", 
+#  :bairro=>"Boa Vista", 
+#  :localidade=>"Recife", 
+#  :uf=>"PE", 
+#  :cep=>"50060002"}]
 ```
--- lib/cep.rb
+++ lib/cep.rb
@@ -6,19 +6,18 @@ class Cep
 
   URL = 'http://m.correios.com.br/movel/buscaCepConfirma.do'
 
+  def initialize
+    @ceps = []
+    @_cep = {}
+  end
+
   def address(cep)
     response = query({cepEntrada: cep, metodo: :buscarCep})
     values = values(response)
+  end
 
-    city_and_state = values[2].split('/').map{|v| v.strip}
-
-    {
-      logradouro: values[0],
-      bairro: values[1],
-      localidade: city_and_state[0],
-      estado: city_and_state[1],
-      cep: values[3]
-    }
+  def cep(cep)
+    address(cep)[0]  
   end
 
   def url(params = {})
@@ -33,10 +32,74 @@ class Cep
   end
 
   def values(response)
-    Nokogiri::HTML(response.body).css('.respostadestaque').map do |el|
-      el.content.gsub(/[\n\t]/,'').strip.gsub(/\s+/,' ')
+    Nokogiri::HTML(response.body).css('.respostadestaque').each do |el|
+      el.parent.children.children.each_slice(2).each do |k,v|
+        elements = clean_element(k, v)
+        handle_keys(elements)
+      end 
+    end
+    @ceps.uniq
+  end
+
+  private
+
+  def clean_element(key, value)
+    key = resolve_text(key.text).gsub(/:/,'').downcase unless key.nil?
+    value = resolve_text(value.text) unless value.nil?
+    [key, value]
+  end
+
+  def resolve_text(text)
+    text.gsub(/[\n\t]/,'').gsub(/\s+/,' ').strip
+  end
+
+  def handle_keys(elements)
+    key = elements[0]
+    value = elements[1]
+    unless key == 'opções' and value == nil
+      add_cep_in_ceps(key)
+      resolve_adrress(key, value)
+      resolve_locality(key, value)
+      resolve_others(key, value)
     end
   end
 
+  def add_cep_in_ceps(key)
+    if key_name_present?(key, ['logradouro','endereço'])
+      @ceps << @_cep unless @_cep.empty?
+      @_cep = {}
+    end
+    @ceps
+  end
 
+  def resolve_adrress(key, value)
+    if key_name_present?(key, ['logradouro','endereço'])
+      @_cep[:tipo_logradouro] = value.split(' ').map{|v| v.strip}[0]
+      @_cep[:logradouro] = value
+    end
+    @_cep
+  end
+
+  def resolve_locality(key, value)
+    if key_name_present?(key, ['localidade / uf'])
+      @_cep[locality(key,0).to_sym] = locality(value,0)
+      @_cep[locality(key,1).to_sym] = locality(value,1)
+    end
+    @_cep
+  end
+
+  def resolve_others(key, value)
+    unless key_name_present?(key, ['logradouro','endereço','localidade / uf'])
+      @_cep[key.to_sym] = value
+    end
+    @_cep
+  end
+
+  def locality(el, pos)
+    el.split('/').map{|v| v.strip}[pos]
+  end
+
+  def key_name_present?(key, names=[])
+    names.include?(key)
+  end
 end
